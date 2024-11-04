3e353989d54e8232f484c19ba8ee55281801afdc
Andrei Cracanau <andithemudkip@gmail.com>
2024-11-04 18:39:25 +0200

    feat: save and load auth status from localstorage

--- dist/core.cjs.js
+++ dist/core.cjs.js
@@ -9497,7 +9497,9 @@ const schemeRegex = /^([a-zA-Z][a-zA-Z0-9+-.]*)$/;
 var constants = {
   SIWE_API: 'http://localhost:3000',
   SIWE_API_VERIFY: 'http://localhost:3000/verify',
-  SIWE_API_NONCE: 'http://localhost:3000/nonce'
+  SIWE_API_NONCE: 'http://localhost:3000/nonce',
+  SIWE_MESSAGE_STATEMENT: 'Connect with Camp Network',
+  AUTH_HUB_BASE_API: 'http://localhost:3000'
 };
 
 /**
@@ -9508,28 +9510,51 @@ var constants = {
 class Auth {
   /**
    * Constructor for the Auth class.
-   * @param {object} options - The options object.
-   * @param {string} options.clientId - The client ID.
+   * @param {object} options The options object.
+   * @param {string} options.clientId The client ID.
+   * @param {string} options.redirectUri The redirect URI used for oauth.
    * @throws {APIError} - Throws an error if the clientId is not provided.
    */
   constructor({
-    clientId
+    clientId,
+    redirectUri
   }) {
     if (!clientId) {
       throw new APIError("clientId is required");
     }
     this.viem = getClient();
     this.clientId = clientId;
+    this.redirectUri = redirectUri;
     this.isAuthenticated = false;
     this.walletAddress = null;
+    this.userId = null;
+    this.#loadAuthStatusFromStorage();
+  }
+
+  /**
+   * Load the authentication status from local storage.
+   * @private
+   * @returns {void}
+   */
+  #loadAuthStatusFromStorage() {
+    const walletAddress = localStorage.getItem("camp-sdk:wallet-address");
+    const userId = localStorage.getItem("camp-sdk:user-id");
+    if (walletAddress && userId) {
+      this.walletAddress = walletAddress;
+      this.userId = userId;
+      this.isAuthenticated = true;
+    } else {
+      this.isAuthenticated = false;
+    }
   }
 
   /**
    * Request the user to connect their wallet.
-   * @returns {Promise<void>} - A promise that resolves when the user connects their wallet.
+   * @private
+   * @returns {Promise<void>} A promise that resolves when the user connects their wallet.
    * @throws {APIError} - Throws an error if the user does not connect their wallet.
    */
-  async requestAccount() {
+  async #requestAccount() {
     try {
       const [account] = await this.viem.requestAddresses();
       this.walletAddress = account;
@@ -9541,10 +9566,11 @@ class Auth {
 
   /**
    * Fetch the nonce from the server.
-   * @returns {Promise<string>} - A promise that resolves with the nonce.
+   * @private
+   * @returns {Promise<string>} A promise that resolves with the nonce.
    * @throws {APIError} - Throws an error if the nonce cannot be fetched.
    */
-  async fetchNonce() {
+  async #fetchNonce() {
     try {
       const res = await fetch(constants.SIWE_API_NONCE, {
         method: "GET",
@@ -9561,12 +9587,13 @@ class Auth {
 
   /**
    * Verify the signature.
-   * @param {string} message - The message.
-   * @param {string} signature - The signature.
-   * @returns {Promise<object>} - A promise that resolves with the verification result.
+   * @private
+   * @param {string} message The message.
+   * @param {string} signature The signature.
+   * @returns {Promise<object>} A promise that resolves with the verification result.
    * @throws {APIError} - Throws an error if the signature cannot be verified.
    */
-  async verifySignature(message, signature) {
+  async #verifySignature(message, signature) {
     try {
       const res = await fetch(constants.SIWE_API_VERIFY, {
         method: "POST",
@@ -9587,14 +9614,15 @@ class Auth {
 
   /**
    * Create the SIWE message.
-   * @param {string} nonce - The nonce.
-   * @returns {string} - The message.
+   * @private
+   * @param {string} nonce The nonce.
+   * @returns {string} The EIP-4361 formatted message.
    */
-  createMessage(nonce) {
+  #createMessage(nonce) {
     return createSiweMessage({
       domain: window.location.host,
       address: this.walletAddress,
-      statement: "Connect with Camp Network",
+      statement: constants.SIWE_MESSAGE_STATEMENT,
       uri: window.location.origin,
       version: "1",
       chainId: this.viem.chain.id,
@@ -9603,24 +9631,39 @@ class Auth {
   }
 
   /**
+   * Disconnect the user.
+   * @returns {void}
+   */
+  async disconnect() {
+    this.isAuthenticated = false;
+    this.walletAddress = null;
+    this.userId = null;
+    localStorage.removeItem("camp-sdk:wallet-address");
+    localStorage.removeItem("camp-sdk:user-id");
+  }
+
+  /**
    * Connect the user's wallet and sign the message.
-   * @returns {Promise<object>} - A promise that resolves with the authentication result.
+   * @returns {Promise<object>} A promise that resolves with the authentication result.
    * @throws {APIError} - Throws an error if the user cannot be authenticated.
    */
-  async sign() {
+  async connect() {
     try {
       if (!this.walletAddress) {
-        await this.requestAccount();
+        await this.#requestAccount();
       }
-      const nonce = await this.fetchNonce();
-      const message = this.createMessage(nonce);
+      const nonce = await this.#fetchNonce();
+      const message = this.#createMessage(nonce);
       const signature = await this.viem.signMessage({
         account: this.walletAddress,
         message: message
       });
-      const res = await this.verifySignature(message, signature, nonce);
+      const res = await this.#verifySignature(message, signature, nonce);
       if (res.success) {
         this.isAuthenticated = true;
+        this.userId = res.userId;
+        localStorage.setItem("camp-sdk:wallet-address", this.walletAddress);
+        localStorage.setItem("camp-sdk:user-id", this.userId);
         return {
           success: true,
           message: "Successfully authenticated",
--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,6 @@ export default {
     SIWE_API: 'http://localhost:3000',
     SIWE_API_VERIFY: 'http://localhost:3000/verify',
     SIWE_API_NONCE: 'http://localhost:3000/nonce',
+    SIWE_MESSAGE_STATEMENT: 'Connect with Camp Network',
+    AUTH_HUB_BASE_API: 'http://localhost:3000',
 }
