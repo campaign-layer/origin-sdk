6fb3e743643cf058605f42eacdd876f9730e034c
Andrei Cracanau <andithemudkip@gmail.com>
2024-11-05 03:40:49 +0200

    fix: remove log

--- src/auth/index.js
+++ src/auth/index.js
@@ -193,7 +193,6 @@ class Auth {
       });
       const res = await this.#verifySignature(message, signature, nonce);
       if (res.success) {
-        console.log(res);
         this.isAuthenticated = true;
         this.userId = res.userId;
         this.jwt = res.token;
