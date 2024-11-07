04b5fcad4168e6e8a130f59109e9de1e0531d075
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-11-07 20:48:33 +0530

    fix(react/auth): walletconnect

--- src/react/auth/modals.jsx
+++ src/react/auth/modals.jsx
@@ -10,6 +10,8 @@ import { ModalContext } from "../context/ModalContext";
 import styles from "./styles/auth.module.css";
 import { CampContext } from "../context/CampContext";
 import { formatAddress } from "../../utils";
+import { useWalletConnectProvider } from "../../auth/viem/walletconnect";
+
 const DiscordIcon = () => (
   <svg
     className="w-8 h-8"
@@ -112,16 +114,26 @@ const ProviderButton = ({ provider, handleConnect, loading }) => {
  * @param { { injectButton: boolean } } props The props.
  * @returns { JSX.Element } The CampModal component.
  */
-export const CampModal = ({ injectButton = true }) => {
+export const CampModal = ({ injectButton = true, projectId }) => {
   const { authenticated, loading } = useAuthState();
   const { connect } = useConnect();
   const { setProvider } = useProvider();
   const { isAuthVisible, setIsAuthVisible, setIsMyCampVisible } =
     useContext(ModalContext);
   const providers = useProviders();
+
+  const walletConnectProvider = useWalletConnectProvider(
+    projectId ?? "1505a4097d6aaa6dcaf284778b30f2f0"
+  );
+
   const handleConnect = (provider) => {
     if (provider) setProvider(provider);
     connect();
+  };
+
+  const handleWalletConnect = async ({ provider }) => {
+    console.log(provider);
+    await provider.connect();
   };
 
   const handleModalButton = () => {
@@ -212,6 +224,17 @@ export const CampModal = ({ injectButton = true }) => {
                     info: { name: "Browser Wallet" },
                   }}
                   handleConnect={handleConnect}
+                  loading={loading}
+                />
+                <ProviderButton
+                  provider={{
+                    provider: walletConnectProvider,
+                    info: {
+                      name: "WalletConnect",
+                      icon: "data:image/svg+xml,%3Csvg fill='%233B99FC' role='img' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.913 7.519c3.915-3.831 10.26-3.831 14.174 0l.471.461a.483.483 0 0 1 0 .694l-1.611 1.577a.252.252 0 0 1-.354 0l-.649-.634c-2.73-2.673-7.157-2.673-9.887 0l-.694.68a.255.255 0 0 1-.355 0L4.397 8.719a.482.482 0 0 1 0-.693l.516-.507Zm17.506 3.263 1.434 1.404a.483.483 0 0 1 0 .694l-6.466 6.331a.508.508 0 0 1-.709 0l-4.588-4.493a.126.126 0 0 0-.178 0l-4.589 4.493a.508.508 0 0 1-.709 0L.147 12.88a.483.483 0 0 1 0-.694l1.434-1.404a.508.508 0 0 1 .709 0l4.589 4.493c.05.048.129.048.178 0l4.589-4.493a.508.508 0 0 1 .709 0l4.589 4.493c.05.048.128.048.178 0l4.589-4.493a.507.507 0 0 1 .708 0Z'/%3E%3C/svg%3E",
+                    },
+                  }}
+                  handleConnect={handleWalletConnect}
                   loading={loading}
                 />
               </div>
