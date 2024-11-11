eb8c08871810b13cd3d42507d05e0aa27b285a48
Andrei Cracanau <andithemudkip@gmail.com>
2024-11-11 18:19:35 +0200

    fix: remove useAuth from react/index

--- src/react/index.js
+++ src/react/index.js
@@ -1,3 +1,2 @@
 import { CampProvider, CampContext } from "../react/context/CampContext.jsx";
-import { useAuth } from "./auth/index.jsx";
-export { CampProvider, CampContext, useAuth };
+export { CampProvider, CampContext };
