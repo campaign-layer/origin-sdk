89ad3bb295bab539a3c2c71a91651b69f5916795
andithemudkip <andithemudkip@gmail.com>
2024-11-15 03:29:12 +0200

    fix: bundle walletconnect lib

--- rollup.config.js
+++ rollup.config.js
@@ -5,33 +5,70 @@ const babel = require("@rollup/plugin-babel");
 const json = require("@rollup/plugin-json");
 const nodePolyfills = require("rollup-plugin-polyfill-node");
 
-const config = {
-  input: "src/index.js",
-  output: [
-    {
-      file: "dist/core.cjs.js",
-      format: "cjs",
-      exports: "auto",
-      inlineDynamicImports: true,
-    },
-    {
-      file: "dist/core.esm.js",
-      format: "esm",
-      exports: "auto",
-      inlineDynamicImports: true,
-    },
-  ],
-  plugins: [
-    resolve({
-      preferBuiltins: false,
-      browser: true,
-    }),
-    commonjs(),
-    babel({ babelHelpers: "bundled" }),
-    json(),
-    nodePolyfills(),
-  ],
-  external: ["react", "react-dom", "@tanstack/react-query", "axios", "viem", "@walletconnect/ethereum-provider"],
-};
+const config = [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/core.cjs.js",
+        format: "cjs",
+        exports: "auto",
+        inlineDynamicImports: true,
+      },
+      {
+        file: "dist/core.esm.js",
+        format: "esm",
+        exports: "auto",
+        inlineDynamicImports: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        preferBuiltins: false,
+        browser: true,
+      }),
+      commonjs(),
+      babel({ babelHelpers: "bundled" }),
+      json(),
+      nodePolyfills(),
+    ],
+    external: [
+      "react",
+      "react-dom",
+      "@tanstack/react-query",
+      "axios",
+      "viem",
+      "@walletconnect/ethereum-provider",
+    ],
+  },
+  {
+    input: "src/auth/viem/walletconnect.js",
+    output: [
+      {
+        file: "dist/auth/viem/walletconnect.cjs.js",
+        format: "cjs",
+        exports: "auto",
+        inlineDynamicImports: true,
+      },
+      {
+        file: "dist/auth/viem/walletconnect.esm.js",
+        format: "esm",
+        exports: "auto",
+        inlineDynamicImports: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        preferBuiltins: false,
+        browser: true,
+      }),
+      commonjs(),
+      babel({ babelHelpers: "bundled" }),
+      json(),
+      nodePolyfills(),
+    ],
+    external: ["react", "react-dom"],
+  },
+];
 
 module.exports = config;
