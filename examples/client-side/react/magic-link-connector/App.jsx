5ba5a0b82705c98e2cdaf4268aa93f55438cb438
Andrei Cracanau <andithemudkip@gmail.com>
2025-01-13 17:47:06 +0200

    feat: add magic.link example

---
+++ examples/client-side/react/magic-link-connector/App.jsx
@@ -0,0 +1,64 @@
+import { CampModal } from "@campnetwork/sdk/react";
+import { Magic } from "magic-sdk";
+import { useEffect, useState } from "react";
+function App() {
+  // Initialize Magic
+  const magic = new Magic("your-magic-key");
+
+  const [provider, setProvider] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // Function to handle Magic login/logout, and set the provider accordingly
+  const handleMagicLogin = async () => {
+    try {
+      setLoading(true);
+      if (provider) {
+        await magic.user.logout();
+        setProvider(null);
+      } else {
+        await magic.wallet.connectWithUI();
+        setProvider(magic.rpcProvider);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    magic.user.isLoggedIn().then((isLoggedIn) => {
+      // If the user is already logged in, set the provider
+      if (isLoggedIn) {
+        setProvider(magic.rpcProvider);
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  // Render the login/logout button and the CampModal component
+  // ! Make sure to only pass the provider to the CampModal component if the user is logged in !
+  
+  return (
+    <div>
+      <button disabled={loading} onClick={handleMagicLogin}>
+        {loading ? "loading" : provider ? "Magic Logout" : "Magic Login"}
+      </button>
+      <br />
+      <br />
+      <CampModal
+        defaultProvider={{
+          provider: provider,
+          info: {
+            name: "Magic", // You can use any name and icon you want
+            icon: "https://magic.link/favicon.ico",
+          },
+          exclusive: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/client-side/react/magic-link-connector/README.md
@@ -0,0 +1,3 @@
+# Camp SDK x Vite x Magic.Link example
+
+This is an example project that demonstrates how to use the Camp SDK with Vite and Magic.Link.
